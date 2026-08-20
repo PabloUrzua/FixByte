8198df936e2f39be450942109f81fec455c4a511
Pablo Urzúa <72765724+PabloUrzua@users.noreply.github.com>
2026-08-20 15:28:39 -0400

    chore: add process.env fallback for vercel

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,8 +3,8 @@ import type { AstroCookies } from 'astro';
 
 export const createSupabaseClient = (cookies: AstroCookies) => {
   return createServerClient(
-    import.meta.env.PUBLIC_SUPABASE_URL,
-    import.meta.env.PUBLIC_SUPABASE_ANON_KEY,
+    import.meta.env.PUBLIC_SUPABASE_URL || process.env.PUBLIC_SUPABASE_URL,
+    import.meta.env.PUBLIC_SUPABASE_ANON_KEY || process.env.PUBLIC_SUPABASE_ANON_KEY,
     {
       cookies: {
         get(key) {
